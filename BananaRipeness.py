ba41d852e60c622b59ff135dbd520e5896943eae
nverkler <nverkler@gmail.com>
2018-09-09 16:13:37 -0500

    Added in code for hue/getting RGB colors

--- BananaRipeness.py
+++ BananaRipeness.py
@@ -1,12 +1,15 @@
-def ban():
+def banana():
     # [START vision_quickstart]
     import io
     import os
+    import webcolors
+
     # Imports the Google Cloud client library
     # [START vision_python_migration_import]
     from google.cloud import vision
     from google.cloud.vision import types
     # [END vision_python_migration_import]
+
     # Instantiates a client
     # [START vision_python_migration_client]
     client = vision.ImageAnnotatorClient()
@@ -14,7 +17,8 @@ def ban():
     # The name of the image file to annotate
     file_name = os.path.join(
         os.path.dirname(__file__),
-        'test.jpg')
+        'Resources/bananaPic.jpg')
+
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
         content = image_file.read()
@@ -29,6 +33,51 @@ def ban():
         print(label.description)
     # [END vision_quickstart]
 
+    response = client.image_properties(image=image)
+    props = response.image_properties_annotation
+    print('Properties:')
+
+    colorMax = 0
+    mostColor = "N/A"
+
+    for color in props.dominant_colors.colors:
+        x = format(color.pixel_fraction)
+
+        if x > colorMax:
+            colorMax = x
+            mostColor = color.color
+
+    R = mostColor.red / 255
+    G = mostColor.green / 255
+    B = mostColor.blue / 255
+
+    hue = 0;
+
+    theMax = max(R, G, B)
+    theMin = min(R,G,B)
+
+    maxMin = theMax - theMin
+
+    if maxMin > 0:
+        if theMax == R:
+            hue = (G-B)/(theMax-theMin)
+        if theMax == G:
+            hue = 2.0 + (B-R)/(theMax-theMin)
+        if theMax == B:
+            hue = 4.0 + (R-G)/(theMax-theMin)
+
+        hue = hue * 60
+
+        if hue < 0:
+            hue = hue + 360
+
+    print hue
+
+
+    print("TESTING THIS: " + str(mostColor.red))
+    print("TESTING THIS: " + str(mostColor.green))
+    print("TESTING THIS: " + str(mostColor.blue))
+    print webcolors.rgb_to_name((str(mostColor.red), str(mostColor.green), str(mostColor.blue)))
 
 if __name__ == '__main__':
-    ban()
+    banana()
